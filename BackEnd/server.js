5ca2bf590ba9d0d0a9badefa2f105daf9bd57a37
sahan <sahan.m.rajapaksha@gmail.com>
2023-11-10 14:04:35 +0530

    initial commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,4 +2,4 @@ const express = require('express'); // Import express
 
 const app = express(); // Make an instance of express
 
-app.listen(5000 , console.log("Server is running on port 5000")); // Make the server on port 5000 (you can enter any port you want )
+app.listen(5000 , console.log("Server is running on port 5000")); // Make the server on port 5000 (you can enter any port you want.any port that is not used by another application)

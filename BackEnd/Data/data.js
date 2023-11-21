a62340517869a795ae4eca83eedc81604229f79b
sahan <sahan.m.rajapaksha@gmail.com>
2023-11-21 12:18:22 +0530

    database Updates

--- BackEnd/Data/data.js
+++ BackEnd/Data/data.js
@@ -1,33 +1,141 @@
 
-  const users =  [
-    {
-      "id": "1",
-      "username": "john_doe",
-      "full_name": "John Doe",
-      "email": "john.doe@example.com",
-      "age": 30,
-      "address": {
-        "street": "123 Main St",
-        "city": "Cityville",
-        "state": "CA",
-        "zip_code": "12345"
-      }
+  // const users =  [
+  //   {
+  //     "id": "1",
+  //     "username": "john_doe",
+  //     "full_name": "John Doe",
+  //     "email": "john.doe@example.com",
+  //     "age": 30,
+  //     "address": {
+  //       "street": "123 Main St",
+  //       "city": "Cityville",
+  //       "state": "CA",
+  //       "zip_code": "12345"
+  //     }
+  //   },
+  //   {
+  //     "id": "2",
+  //     "username": "jane_smith",
+  //     "full_name": "Jane Smith",
+  //     "email": "jane.smith@example.com",
+  //     "age": 25,
+  //     "address": {
+  //       "street": "456 Oak St",
+  //       "city": "Townsville",
+  //       "state": "NY",
+  //       "zip_code": "67890"
+  //     }
+  //   }
+  // ]
+  const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anthony",
+        email: "anthony@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
     },
-    {
-      "id": "2",
-      "username": "jane_smith",
-      "full_name": "Jane Smith",
-      "email": "jane.smith@example.com",
-      "age": 25,
-      "address": {
-        "street": "456 Oak St",
-        "city": "Townsville",
-        "state": "NY",
-        "zip_code": "67890"
-      }
-    }
-  ]
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Jane Doe",
+        email: "jane@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
 
 
 
-module.exports = users;
+module.exports = chats;
--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -18,7 +18,7 @@ const ChatPage = () => {
   return (
     <div>
       {chats.map((chat) => (
-        <div key={chat.id}>{chat.full_name}</div>
+        <div key={chat._id}>{chat.chatName}</div>
       ))}
     </div>
   );
